7987b70a00d4035f725f9bc33057523f0c65cd90
Bill <williamvinicius007@gmail.com>
2025-09-05 11:58:22 -0300

    aula

--- Integração com Banco de Dados/Agosto/Aula 2/consulta.sql
+++ Integração com Banco de Dados/Agosto/Aula 2/consulta.sql
@@ -7,7 +7,21 @@
 --FROM invoices 
 --WHERE BillingCity = 'Brasília';
 
-SELECT t.Name 
-FROM tracks t
-INNER JOIN genres g ON t.GenreId = g.GenreId
-WHERE g.Name = 'Rock And Roll';
+--SELECT t.Name 
+--FROM tracks t
+--INNER JOIN genres g ON t.GenreId = g.GenreId
+--WHERE g.Name = 'Rock And Roll';
+
+.mode column
+
+.once saida.txt
+--SELECT artists.Name AS Artista
+--FROM albums 
+--INNER JOIN artists ON albums.ArtistId = artists.ArtistId
+--WHERE albums.Title = 'Dark Side Of The Moon';
+
+SELECT albums.Title AS Album, artists.Name AS Artista
+FROM albums 
+INNER JOIN artists ON albums.ArtistId = artists.ArtistId
+WHERE albums.Title LIKE '%black%' OR artists.Name LIKE '%black%';
+
